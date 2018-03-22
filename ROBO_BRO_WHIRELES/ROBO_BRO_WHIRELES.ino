8779fc1074f4a3b783af5e788e105d385f335789
Serhii Bilovol <serhiibilovol@gmail.com>
2018-03-22 19:28:43 +0200

    Fix for wheels

--- ROBO_BRO_WHIRELES/ROBO_BRO_WHIRELES.ino
+++ ROBO_BRO_WHIRELES/ROBO_BRO_WHIRELES.ino
@@ -24,11 +24,6 @@ int LEFTWHEEL_BACKWARD = 2;*/
 #define POWER_RIGHT 6
 #define POWER_LEFT 3
 
-const byte COMMAND_LEFT =  1;
-const byte COMMAND_FORWARD =  2;
-const byte COMMAND_RIGHT =  3;
-const byte COMMAND_BACKWARD =  4;
-
 void setup()
 {
   Serial.begin(9600);
@@ -56,81 +51,24 @@ void moveBot() {
   bool isAvailable = receiveEngineState(leftState, leftPower, rightState, rightPower);
 
   if(isAvailable) {
-    Serial.write(leftState);
-    Serial.write(leftPower);
-    Serial.write(rightState);
-    Serial.write(rightPower);
-    Serial.write("\n");
-    
+//    Serial.write(leftState);
+//    Serial.write(leftPower);
+//    Serial.write(rightState);
+//    Serial.write(rightPower);
+//    Serial.write("\n");
+//    
     Move(leftState, leftPower, rightState, rightPower);
-//    leftEngine(leftState, leftPower);
-//    rightEngine(rightState, rightPower);
   }
 }
 
-//void leftEngine(byte state, byte power) {  
-//  if(state == 1)  {
-//    digitalWrite(LEFT_BACKWARD, 0);
-//    digitalWrite(LEFT_FORWARD, power);
-//  } else if(state == 0) {
-//    digitalWrite(LEFT_FORWARD, 0);
-//    digitalWrite(LEFT_BACKWARD, power);
-//  }
-//}
-//
-//void rightEngine(byte state, byte power) {
-//  if(state == 1)  {
-//    digitalWrite(RIGHT_BACKWARD, 0);
-//    digitalWrite(RIGHT_FORWARD, power);    
-//  } else if(state == 0) {
-//    digitalWrite(RIGHT_FORWARD, 0);
-//    digitalWrite(RIGHT_BACKWARD, power);
-//  }
-//}
-//
-//void MoveCommand(byte &command)
-//{
-//  if (command == COMMAND_LEFT)
-//    {
-//      printf("COMMAND_LEFT\n");
-//      MoveEngine(RIGHT_FORWARD, LEFT_BACKWARD);
-//    }
-//    else if (command == COMMAND_FORWARD)
-//    {
-//      printf("COMMAND_FORWARD\n");
-//      MoveEngine(RIGHT_FORWARD, LEFT_FORWARD);
-//    }
-//    else if (command == COMMAND_RIGHT)
-//    {
-//      printf("COMMAND_RIGHT\n");
-//      MoveEngine(RIGHT_BACKWARD, LEFT_FORWARD);
-//    }
-//    else if (command == COMMAND_BACKWARD)
-//    {
-//      printf("COMMAND_BACKWARD\n");
-//      MoveEngine(RIGHT_BACKWARD, LEFT_BACKWARD);
-//    }
-//}
-
-//void MoveEngine(int rightEngine, int leftEngine)
-//{
-//  digitalWrite(rightEngine, HIGH);
-//  digitalWrite(leftEngine, HIGH);
-//  delay(10);
-//  digitalWrite(rightEngine, LOW);
-//  digitalWrite(leftEngine, LOW);
-//  delay(10);
-//}
-
 void Move(byte &leftState, byte &leftPower, byte &rightState, byte &rightPower)
 {
-    analogWrite(POWER_LEFT, leftPower);   
-    digitalWrite(LEFT_BACKWARD, !leftState);
-    digitalWrite(LEFT_FORWARD, leftState);
-
+    analogWrite(POWER_LEFT, leftPower);
+    digitalWrite(LEFT_FORWARD, leftState);   
+    digitalWrite(LEFT_BACKWARD, !leftState); 
 
     analogWrite(POWER_RIGHT, rightPower);
-    digitalWrite(LEFT_FORWARD, rightState);
-    digitalWrite(LEFT_BACKWARD, !rightState);
+    digitalWrite(RIGHT_FORWARD, rightState);
+    digitalWrite(RIGHT_BACKWARD, !rightState);
 }
 
